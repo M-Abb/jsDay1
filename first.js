70782da5388cd032d30c3cee7d7fe38284affdcd
M-ABB <rizviabbas141@gmail.com>
2025-05-26 03:29:40 +0500

    Third Commit

--- first.js
+++ first.js
@@ -1,3 +1,12 @@
-let a="1"+2+2;
-let b=1+2+"9";
-console.table([a,b]);
+let firstUser = "SYED MUHAMMAD ABBAS RIZVI";
+console.log(firstUser);
+let secondUser="SYED AZHAR ABBAS RIZVI";
+console.log(secondUser);
+let user1={
+   name:"ABBAS",
+   mail:"abbasrizvi141@gmail.com",
+}
+let user2=user1;
+user2.mail="ShayanAbbas121@gmail.com";
+console.log(user1.mail);
+console.log(user2.mail);
